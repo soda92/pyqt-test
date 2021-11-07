f496cc8bf6965404757f705ca835c58710e35348
SodaCris <18463922396@163.com>
2021-11-07 14:38:32 +0800

    adding res and spec files

--- tray.py
+++ tray.py
@@ -3,13 +3,17 @@ import sys
 
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QWidget, QDesktopWidget
-
+from pathlib import Path
+CURR = Path(__file__).resolve().parent
+print(CURR)
+import os
+print(os.listdir(CURR))
 app = QApplication(sys.argv)
 
 w = QWidget()
 w.resize(400, 300)
 w.setWindowTitle('Simple')
-w.setWindowIcon(QIcon('face.png'))
+w.setWindowIcon(QIcon(str(Path.joinpath(CURR,'icon.png'))))
 qtRectangle = w.frameGeometry()
 centerPoint = QDesktopWidget().availableGeometry().center()
 qtRectangle.moveCenter(centerPoint)
